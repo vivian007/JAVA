31c669ca47acde2977b21a8f0f80f588b3c4dd3a
vivian007 <1041264719@qq.com>
2018-05-22 15:01:37 +0800

    5.22日学习提交

---
+++ ZipCode.java
@@ -0,0 +1,25 @@
+public class ZipCode {
+    private String zipCode = "";
+
+    public ZipCode(String inZipCode) {
+        setZipCode(inZipCode);
+    }
+
+    public void setZipCode(String inZipCode) {
+        if ((inZipCode.length() == 5) | (inZipCode.length() == 9))
+            zipCode = inZipCode;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public static void main(String[] arguments) {
+        ZipCode zip1 = new ZipCode("75044");
+        ZipCode zip2 = new ZipCode("320840");
+        if (zip1.getZipCode() != "")
+            System.out.println("Zip1: " + zip1.getZipCode());
+        if (zip2.getZipCode() != "")
+            System.out.println("Zip2: " + zip2.getZipCode());
+    }
+}
